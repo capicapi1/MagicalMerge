c30aaab64890668a1a44696e1ac9a4f1f88a1bce
capicapi1 <m.mexico@hotmail.com>
2026-09-05 18:58:56 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "magicalmerge-v2";
+const CACHE = "magicalmerge-v4";
 const ASSETS = ["./","./index.html","./styles.css","./game.js","./manifest.json","./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
